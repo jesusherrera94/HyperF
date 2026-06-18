60eecd9386143f2a5dc884d8ccb3f707f54fe114
Jesus Herrera <jesusherrera1594@gmail.com>
2026-06-18 09:57:53 -0600

    Initial commit and configs

---
+++ Makefile
@@ -0,0 +1,22 @@
+# Project Name
+TARGET = hyper_f
+
+# Uncomment to use LGPL (like ReverbSc, etc.)
+USE_DAISYSP_LGPL=1
+
+# Library Locations
+HOTHOUSE_DIR ?= ../../HothouseExamples
+LIBDAISY_DIR ?= $(HOTHOUSE_DIR)/libdaisy
+DAISYSP_DIR ?= $(HOTHOUSE_DIR)/DaisySP
+
+# Sources and Hothouse header files
+CPP_SOURCES = $(TARGET).cpp $(HOTHOUSE_DIR)/src/hothouse.cpp
+C_INCLUDES = -I$(HOTHOUSE_DIR)/src/
+
+
+# Core location, and generic Makefile.
+SYSTEM_FILES_DIR = $(LIBDAISY_DIR)/core
+include $(SYSTEM_FILES_DIR)/Makefile
+
+# Global includes
+include $(HOTHOUSE_DIR)/src/Makefile
---
+++ hyper_f.cpp
@@ -0,0 +1,74 @@
+// HyperF for Hothouse DIY DSP Platform
+// Copyright (C) 2024 Jesus Herrera <jesusherrera1594@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// ### Uncomment if IntelliSense can't resolve DaisySP-LGPL classes ###
+// #include "daisysp-lgpl.h"
+
+#include "daisysp.h"
+#include "hothouse.h"
+
+using clevelandmusicco::Hothouse;
+using daisy::AudioHandle;
+using daisy::Led;
+using daisy::SaiHandle;
+
+Hothouse hw;
+
+// Bypass vars
+Led led_bypass;
+bool bypass = true;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
+                   size_t size) {
+  hw.ProcessAllControls();
+
+  // Toggle bypass when FOOTSWITCH_2 is pressed
+  bypass ^= hw.switches[Hothouse::FOOTSWITCH_2].RisingEdge();
+
+  for (size_t i = 0; i < size; ++i) {
+    if (bypass) {
+      // Copy left input to both outputs (mono-to-dual-mono)
+      out[0][i] = out[1][i] = in[0][i];
+    } else {
+      // TODO: replace silence with something awesome
+      out[0][i] = out[1][i] = 0.0f;
+    }
+  }
+}
+
+int main() {
+  hw.Init();
+  hw.SetAudioBlockSize(48);  // Number of samples handled per callback
+  hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+
+  led_bypass.Init(hw.seed.GetPin(Hothouse::LED_2), false);
+
+  hw.StartAdc();
+  hw.StartAudio(AudioCallback);
+
+  while (true) {
+    hw.DelayMs(10);
+
+    // Toggle effect bypass LED when footswitch is pressed
+    led_bypass.Set(bypass ? 0.0f : 1.0f);
+    led_bypass.Update();
+
+    // Call System::ResetToBootloader() if FOOTSWITCH_1 is pressed for 2 seconds
+    hw.CheckResetToBootloader();
+  }
+  
+  return 0;
+}
