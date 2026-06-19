633db151646c3af6a8cd6ebda1bf42e3c5672dee
Jesus Herrera <jesusherrera1594@gmail.com>
2026-06-19 10:38:14 -0600

    fix duplicated imports between hyper_f and HotHouseAdapter files

--- hyper_f.cpp
+++ hyper_f.cpp
@@ -14,11 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-// ### Uncomment if IntelliSense can't resolve DaisySP-LGPL classes ###
-// #include "daisysp-lgpl.h"
-
-#include "daisysp.h"
-#include "hothouse.h"
 #include "FuzzProcessor.h"
 #include "HotHouseAdapter.h"
 #include <memory>
